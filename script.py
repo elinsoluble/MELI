a83269318283a8e3f6132cd5dca882afc315a794
elinsoluble <elinsoluble87@hotmail.com>
2022-03-10 18:41:08 +0000

    Add files via upload

--- script.py
+++ script.py
@@ -2,17 +2,39 @@
 
 import requests
 
+'''
+    Requiere como parametros de entrada el SITE_ID y el SELLER_ID
+
+    SITE_ID es de tipo string
+    SELLER_ID es de tipo entero
+'''
+
 SITE_ID = str(input("SITE ID: "))
 SELLER_ID = int(input("SELLER_ID: "))
 
 #SITE_ID="MLA"
 #SELLER_ID="179571326"
 
+
+'''
+    Se hace una peticion a la API sites de ML utilizando los parametros de entrada
+
+    Luego obtenemos los items del usuario
+'''
+
 req = requests.get(f"https://api.mercadolibre.com/sites/{SITE_ID}/search?seller_id={SELLER_ID}")
 
 info = req.json()
 items = info['results']
 
+
+'''
+    Por cada item del usuario, obtenemos los datos requeridos.
+
+    Para obtener el nombre de la categoria utilizamos el valor de category_id, y una llama a la API de categories
+
+    Luego guardamos en el archivo LOG todos los datos necesarios
+'''
 with open("LOG", 'w') as f:
     for item in items:
         itemID = item['id']
